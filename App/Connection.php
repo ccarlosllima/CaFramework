fc145af1046433773b1fe5b162de2d7ea5990e03
carlos <ccarlos.llima@gmail.com>
2021-08-10 07:34:09 -0300

    atualizado string de conexão com banco de dados

--- App/Connection.php
+++ App/Connection.php
@@ -5,7 +5,7 @@ class Connection {
     public static function getDb(){
         try {
             $conn = new \PDO(
-                'mysql:host=localhost;dbname=mvc','carloslima','admcarlos'
+                'mysql:host=******;dbname=*****','******','********'
             );
             return $conn;
         } catch (\PDOException $e) {
